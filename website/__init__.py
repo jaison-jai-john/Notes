1de2efe6eefd7c2cb522ce9247f9e30a8501345c
jaison-jai-john <jaisonjaison2004@gmail.com>
2024-09-29 18:57:58 +0530

    changes made for deployment. changed app name for convenience

--- website/__init__.py
+++ website/__init__.py
@@ -4,37 +4,39 @@ from flask import Flask
 from flask_login import LoginManager, login_manager
 from flask_sqlalchemy import SQLAlchemy
 
-DB_NAME = 'database.db'
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'akslfyqnoahdjaixhdfanfabu#af'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+DB_NAME = "database.db"
+app = Flask("app")
+app.config["SECRET_KEY"] = "akslfyqnoahdjaixhdfanfabu#af"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
 db = SQLAlchemy(app)
 
+
 def create_app():
-  # db.init_app(app)
-  
-  from .auth import auth
-  from .views import views
-  
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
-  
-  from .models import Note, User
-  
-  create_database(app)
-  
-  login_manager = LoginManager()
-  login_manager.login_view = 'auth.login'
-  login_manager.init_app(app)
-  
-  @login_manager.user_loader
-  def load_user(id):
-    return User.query.get(int(id))
-  
-  return app
+    # db.init_app(app)
+
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .models import Note, User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
 
 def create_database(app):
-  if not path.exists('website' + DB_NAME):
-    with app.app_context():
-      db.create_all()
-      print('created database')
+    if not path.exists("website" + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("created database")
